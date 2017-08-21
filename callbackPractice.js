f074206f79ac51a825e98d302f9d9d5ae0ee3be1
Foster Irwin <jfosterirwin@Fosters-MacBook-Air.local>
2017-08-20 23:03:04 -0600

    completed seventh problem

--- callbackPractice.js
+++ callbackPractice.js
@@ -123,8 +123,13 @@ each(names, function(item, indice){
 // 7. Write a function called getUserById that looks at the array of user objects (users) and searches for a user by ID.
 // When the correct user object is found, invoke the callback with the user object as an argument.
 
-// Code here
-
+function getUserById (arr, id, cb) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].id === id) {
+      cb(arr[i]);
+    }
+  }
+}
 
 
 var users = [
